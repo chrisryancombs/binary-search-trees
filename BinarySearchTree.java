e30b5e881c6dfe84da2a947b1b95dc52a1d0604d
Christopher Ryan Combs Jr <youleavesmealone@gmail.com>
2016-11-23 02:32:25 -0700

    Binary search tree finished

--- BinarySearchTree.java
+++ BinarySearchTree.java
@@ -1,3 +1,5 @@
+import java.util.NoSuchElementException;
+
 public class BinarySearchTree<K extends Comparable<K>, E>  implements IBinarySearchTree<K,E>
 {
 	int numNodes; // Number of nodes in tree total
@@ -56,10 +58,13 @@ public class BinarySearchTree<K extends Comparable<K>, E>  implements IBinarySea
     		} else { // Otherwise check right node
     			current = current.rightChild;
     		}
-    		if(current == null) return null; // If we can't find it, return null
+    		if(current == null) 
+    		{
+    			throw new NoSuchElementException(); // If we can't find it, return null
+    		}
     	}
     	
-    	return (E)current; // Cast to E
+    	return (E) current.element; // Cast to E
     }
 
     public int size()
@@ -69,7 +74,36 @@ public class BinarySearchTree<K extends Comparable<K>, E>  implements IBinarySea
 
     public String toString()
     {
-    	return "";
+    	if(root == null) // Special case: no root
+    	{
+    		return"[]";
+    	}
+    	String keyString = root.toString(); // Save printable version
+    	String elementString = root.element.toString() + ", "; // Element and comma
+    	String leftString = "";
+    	String rightString = "";
+    	
+    	leftString = toStringRecursive(root.leftChild); // Recursive calls on left and right
+    	rightString = toStringRecursive(root.rightChild);
+    	
+    	
+    	String a = "["+leftString  +keyString +": "+ elementString + rightString +"]"; // Concatenate using in-order
+    	return a.substring(0, a.length()-3) + "]"; // Remove last comma
+    }
+    
+    public String toStringRecursive(Node n)
+    {
+    	if(n == null) // Special case: no root
+    	{
+    		return"";
+    	}
+    	String keyString = n.toString(); // Save printable version
+    	String elementString = n.element.toString() + ", "; // Element and comma
+    	String leftString = toStringRecursive(n.leftChild); // More recursive calls
+    	String rightString = toStringRecursive(n.rightChild);
+    	
+    	
+    	return (leftString  +keyString +": "+ elementString + rightString); //Concatenate
     }
 
     public int getHeight() // Returns the height of the tree
@@ -107,8 +141,60 @@ public class BinarySearchTree<K extends Comparable<K>, E>  implements IBinarySea
     }
 
     public String getTreeString()
-    {
-    	return "";
+    {	
+    	if(root == null) // Special case: no root
+    	{
+    		return"[]";
+    	}
+    	String keyString = root.toString(); // Save printable version
+    	String leftString;
+    	String rightString;
+    	
+    	if(root.leftChild == null) // Return null string for each side of null with no node
+    	{
+    		leftString = "null";
+    	} else { 
+    		leftString = getTreeStringRecursive(root.leftChild);
+    	}
+    	
+    	if(root.rightChild == null) // Return null string for each side of null with no node
+    	{
+    		rightString = "null";
+    	} else { 
+    		rightString = getTreeStringRecursive(root.rightChild);
+    	}
+    	
+    	
+    	return ("[" +keyString + ", " + leftString + ", " + rightString + "]");
+    }
+    
+    private String getTreeStringRecursive(Node n)
+    {	
+    	if(n == null) // Base case: return string representation of a null node
+    	{
+    		return "null";
+    	}
+    	
+    	String keyString = n.toString(); // Save printable version
+    	String leftString;
+    	String rightString;
+    	
+    	if(n.leftChild == null) // Add null string for each side of null with no node
+    	{
+    		leftString = "null";
+    	} else { 
+    		leftString = getTreeStringRecursive(n.leftChild);
+    	}
+    	
+    	if(n.rightChild == null) // Add null string for each side of null with no node
+    	{
+    		rightString = "null";
+    	} else { 
+    		rightString = getTreeStringRecursive(n.rightChild);
+    	}
+    	
+    	
+    	return ("[" +keyString + ", " + leftString + ", " + rightString + "]");
     }
 	
     private class Node<K extends Comparable<K>,E>{
@@ -128,12 +214,7 @@ public class BinarySearchTree<K extends Comparable<K>, E>  implements IBinarySea
     	}
     	public String toString()
     	{
-    		return element.toString();
-    	}
-    	
-    	public String toStringRecursive(Node current)
-    	{
-    		return "";
+    		return key.toString();
     	}
     }
 }
